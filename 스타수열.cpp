7de4b474bff9598b95dd4940b2236ae899982e88
2015005050 <dpwl1172@naver.com>
2021-05-26 23:21:17 +0900

    Solve algorithm : impl2

--- 스타수열.cpp
+++ 스타수열.cpp
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <algorithm>
 
-int solution(std::vector<int> a) {
+using namespace std;
+
+int solution(vector<int> a) {
     int answer = 0;
     
-    std::vector<int> arr(a.size()+1, 0);
+    vector<int> arr(a.size()+1, 0);
 
     for (int i = 0; i < a.size(); i++) {
         arr[a[i]]++;
@@ -14,19 +16,19 @@ int solution(std::vector<int> a) {
     
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] == 0) continue;
-        if (arr[i] < answer) continue;
+        if (arr[i] <= answer) continue;
         
         int cnt = 0;
-        for (int j = 0; j < a.size()-1; j+=2) {
-            if (a[j] == i || a[j+1] == i) {
-                if (a[j] != a[j+1]) {
-                    cnt++;
-                }
-            } 
+        for (int j = 0; j < a.size()-1; j++) {
+            if (a[j] != i && a[j + 1] != i) continue;
+            if (a[j] == a[j + 1]) continue;
+            
+            cnt++;
+            j++;
             
         }
         
-        answer = std::max(answer,cnt);
+        answer = max(answer,cnt);
     }
     
     return answer*2;
